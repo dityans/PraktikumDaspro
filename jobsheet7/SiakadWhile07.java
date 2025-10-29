3b771a16cfb45e549888c8ab8b9667ea28a2b6bc
dityans <dityan25@gmail.com>
2025-10-29 09:29:29 +0700

    Modifikasi Tugas2

--- jobsheet7/SiakadWhile07.java
+++ jobsheet7/SiakadWhile07.java
@@ -18,19 +18,20 @@ public class SiakadWhile07 {
             }
             
             if (nilai > 80 && nilai <= 100){
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah A");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya");
             } else if (nilai > 73 && nilai <= 80){
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah B+");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah B+");
             } else if (nilai > 65 && nilai <= 73){
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah B");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah B");
             } else if (nilai > 60 && nilai <= 65){
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah C+");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah C+");
             }else if (nilai > 50 && nilai <= 60){
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah C");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah C");
             }else if (nilai > 39 && nilai <= 50){
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah D");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah D");
             } else {
-                System.out.println("Nilai mahasiswa ke-" + i + "adalah E");
+                System.out.println("Nilai mahasiswa ke-" + i + " adalah E");
             }
             i++;
         }
