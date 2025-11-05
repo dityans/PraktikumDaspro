1c1d350856d6f4e639a3ab818f42687a796424bc
dityans <dityan25@gmail.com>
2025-11-05 10:45:11 +0700

    revisi tugas pseudo

--- jobsheet8/tugas2.java
+++ jobsheet8/tugas2.java
@@ -1,8 +1,9 @@
 public class tugas2 {
        public static void main(String[] args) {
-        int i, j,n, k = 0;
+        int i, j,n;
         for (i = 1; i<=5; i++) {
             System.out.print("n = " + i + " -> jumlah kuadrat = ");
+            int k = 0;
             for (j = 1; j <= i; j++) {
                 n = (j*j);
                 k += n;
@@ -14,10 +15,11 @@ public class tugas2 {
             }
             if (k != 1) {
                 System.out.print(" = " + k);
-                k=0;
                 System.out.println();
             }
-            else {System.out.println();}
+            else {
+                System.out.println();
+            }
         }
     } 
 }
