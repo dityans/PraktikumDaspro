8108bf2a6600b306f8fb0fc72a819412f1dc025f
dityans <dityan25@gmail.com>
2025-11-09 13:26:08 +0700

    tugas jobsheet

--- jobsheet8/tugas1Jobsheet.java
+++ jobsheet8/tugas1Jobsheet.java
@@ -1,13 +1,17 @@
-public class tugas2 {
+import java.util.Scanner;
+public class tugas1Jobsheet {
        public static void main(String[] args) {
-        int i, j,n;
-        for (i = 1; i<=5; i++) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan nilai n: ");
+        int n = input.nextInt();
+        int i, j, kuadrat;
+        for (i = 1; i<=n; i++) {
             System.out.print("n = " + i + " -> jumlah kuadrat = ");
             int total = 0;
             for (j = 1; j <= i; j++) {
-                n = (j*j);
-                total += n;
-                System.out.print(n);
+                kuadrat = (j*j);
+                total += kuadrat;
+                System.out.print(kuadrat);
 
                 if (j<i){
                 System.out.print( " + ");
@@ -20,5 +24,6 @@ public class tugas2 {
                 System.out.println();
             }
         }
+        input.close();
     } 
 }
