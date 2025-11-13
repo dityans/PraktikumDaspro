d0b6e20550fa6df5c4a85d2efaca6200781b6322
dityans <dityan25@gmail.com>
2025-11-13 16:34:44 +0700

    Modifikasi kondisi perulangan

--- jobsheet9/ArrayNilai07.java
+++ jobsheet9/ArrayNilai07.java
@@ -4,11 +4,11 @@ public class ArrayNilai07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilai = new int[10];
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + ": ");
             nilai[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilai.length; i++) {
             System.out.println("Nilai akhir ke-" + i + " adalah: " + nilai[i]);
         }
         sc.close();
