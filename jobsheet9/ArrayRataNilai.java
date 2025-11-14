a08d5850d33225807d632617517883a3cbe9d276
dityans <dityan25@gmail.com>
2025-11-14 08:42:26 +0700

    Modifikasi percobaan 3, output nilai tertinggi dan terendah

--- jobsheet9/ArrayRataNilai.java
+++ jobsheet9/ArrayRataNilai.java
@@ -12,23 +12,31 @@ public class ArrayRataNilai {
         double rata2TdkLulus;
         int lulus = 0;
         int tdkLulus = 0;
+        int tertinggi = 0;
+        int terendah = 100;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            if (nilaiMhs[i]> 70){
+            if (nilaiMhs[i]> 70){               //nilai diatas 70 lulus
                 totalLulus += nilaiMhs[i];
                 lulus++;
-            }else if (nilaiMhs[i] <= 70){
+            }else if (nilaiMhs[i] <= 70){       //nilai 70 ke bawah tidak lulus
                 totalTdkLulus += nilaiMhs[i];
                 tdkLulus++;
+            }if (nilaiMhs[i] > tertinggi){      //mencari nilai tertinggi
+                tertinggi = nilaiMhs[i];
+            }else if (nilaiMhs[i] < terendah){  //mencari nilai terendah
+                terendah = nilaiMhs[i];
             }
         }
         rata2Lulus = totalLulus / lulus;
         rata2TdkLulus = totalTdkLulus / tdkLulus;
         System.out.println("Rata-rata nilai lulus= " + rata2Lulus );
         System.out.println("Rata-rata nilai tidak lulus= " + rata2TdkLulus );
+        System.out.println("Nilai tertinggi= " + tertinggi);
+        System.out.println("Nilai terendah= " + terendah);
         sc.close();
     }
 }
