a286b3705d4ddcee114b45759b24e74399b367d1
dityans <dityan25@gmail.com>
2025-11-16 13:15:04 +0700

    Tugas Jobsheet 9

---
+++ jobsheet9/TugasJobsheet9.java
@@ -0,0 +1,38 @@
+package jobsheet9;
+import java.util.Scanner;
+public class TugasJobsheet9 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String [] menu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"} ;
+        double[] daftarHarga = {20000, 22000, 15000, 17000, 13000, 18000, 20000};
+        double totalPembayaran = 0;
+        System.out.print("Masukan jumlah menu yang ingin diinput: ");
+        int jmlMenu = sc.nextInt();
+        if (jmlMenu == 0){
+            System.out.println("Tidak ada pesanan");
+            sc.close();
+            return;
+        }
+        String[] daftarPesanan = new String[menu.length];
+        int jmlItem = 0;
+        for (int i = 0; i < jmlMenu; i++) {
+            sc.nextLine();
+            System.out.print("Masukan menu ke-" + (i+1) + ": ");
+            String pesanan = sc.nextLine();      
+            for (int j = 0; j < menu.length; j++) {
+                if (pesanan.equalsIgnoreCase(menu[j])) {
+                    daftarPesanan[j] = pesanan;
+                    System.out.print("Masukan jumlah item: ");
+                    jmlItem = sc.nextInt();
+                    totalPembayaran += daftarHarga[j] * jmlItem;
+                    break;
+                } else if (j == menu.length - 1) {
+                    System.out.println("Menu tidak tersedia"); 
+                    i--;
+                    }
+                }            
+            }  
+        System.out.println("Total pembayaran: Rp." + totalPembayaran);
+        sc.close();
+    }
+}
