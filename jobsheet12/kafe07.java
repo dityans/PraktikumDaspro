628d1713a2c555a2c85f09c4c99c583470d2e179
dityans <dityan25@gmail.com>
2025-12-03 09:25:34 +0700

    Modifikasi percobaan 2

--- jobsheet12/kafe07.java
+++ jobsheet12/kafe07.java
@@ -1,24 +1,33 @@
 package jobsheet12;
 
 public class kafe07 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
-    System. out.println("Selamat datang, " + namaPelanggan + "!");
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System. out.println("Selamat datang, " + namaPelanggan + "!");
 
-    if (isMember) {
-    System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-    }
-
-    System. out.println(" ===== MENU RESTO KAFE ===== ");
-    System.out.println("1. Kopi Hitam - Rp 15,000");
-    System.out.println("2. Cappuccino - Rp 20,000");
-    System.out.println("3. Latte - Rp 22,000");
-    System.out.println("4. Teh Tarik - Rp 12,000");
-    System.out.println("5. Roti Bakar - Rp 10,000");
-    System.out.println("6. Mie Goreng - Rp 18,000");
-    System.out.println("==============================");
-    System.out.println("Silakan pilih menu yang Anda inginkan.");
+        if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        String[] daftarKupon = {"DISKON50", "DISKON30"};
+        int[] daftarDiskon = {50, 30};
+        for (int i = 0; i < daftarKupon.length; i++) {
+            if (kodePromo.equalsIgnoreCase(daftarKupon[i])) {
+                System.out.println("Anda mendapat diskon sebanyak " + daftarDiskon[i] + "% dari kode promo " + kodePromo + "!");
+                break;
+            } else if (i == daftarKupon.length - 1) {
+                System.out.println("Kode promo tidak valid.");
+            }
+        }
+        System. out.println(" ===== MENU RESTO KAFE ===== ");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("==============================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
 }
     public static void main(String[] args) {
-       Menu("Andi", true);
+       Menu("Andi", true, "DISKON30");
     }
 }
